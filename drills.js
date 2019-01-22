6f97e618b2829589c0407422373a47744efc3328
Michael McKay <mamckay89@gmail.com>
2019-01-22 09:53:09 -0700

    Working on drills

---
+++ drills.js
@@ -0,0 +1,104 @@
+class HashMap {
+  // Why is there a length and a capacity
+  constructor(initialCapacity = 8) {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      throw new Error("Key error");
+    }
+    return this._slots[index].value;
+  }
+
+  set(key, value) {
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
+
+    const index = this._findSlot(key);
+    this._slots[index] = {
+      key,
+      value,
+      deleted: false
+    };
+    this.length++;
+  }
+
+  remove(key) {
+    const index = this._findSlot(key);
+    const slot = this._slots[index];
+    if (slot === undefined) {
+      throw new Error("Key error");
+    }
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
+  }
+
+  _findSlot(key) {
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
+
+    for (let i = start; i < start + this._capacity; i++) {
+      // Why are we creating this index this way and not using the start variable
+      const index = i % this._capacity;
+      const slot = this._slots[index];
+      if (slot === undefined || (slot.key == key && !slot.deleted)) {
+        return index;
+      }
+    }
+  }
+
+  // go over this a bit
+  _resize(size) {
+    const oldSlots = this._slots;
+    this._capacity = size;
+    // Reset the length - it will get rebuilt as you add the items back
+    this.length = 0;
+    this._deleted = 0;
+    this._slots = [];
+
+    for (const slot of oldSlots) {
+      if (slot !== undefined && !slot.deleted) {
+        this.set(slot.key, slot.value);
+      }
+    }
+  }
+
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      // shift operator - check it out,  bitwise
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
+  }
+}
+
+HashMap.MAX_LOAD_RATIO = 0.9;
+HashMap.SIZE_RATIO = 3;
+
+const testMap = new HashMap();
+
+testMap.set("Hobbit", "Bilbo");
+testMap.set("Zit", "Bilbo");
+testMap.set("bit", "mike");
+testMap.set("Hobit", "Bilbo");
+testMap.set("bbit", "Bilbo");
+testMap.set("Hobbt", "Bilbo");
+
+console.log(testMap.get("bit"));
+
+function palindrome(str) {
+  // return true when we find a single valid palindrome
+  // When we come up with a permutation we store it
+  // Why store failed permutations?
+  //
+}
